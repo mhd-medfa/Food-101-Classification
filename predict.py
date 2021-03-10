8f9f913c2e4f89aa248269ec980b2efebd6c4e1b
mhd-medfa <mohamad.almdfaa@yahoo.com>
2021-03-10 06:40:42 +0200

    clean predict.py

--- predict.py
+++ predict.py
@@ -5,11 +5,6 @@
 # ![alt text](https://www.vision.ee.ethz.ch/datasets_extra/food-101/static/img/food-101.jpg)
 
 
-# ### Download and extract Food 101 Dataset
-
-# In[1]:
-
-
 # Check if GPU is enabled
 import tensorflow as tf
 seed_value=42
@@ -34,7 +29,6 @@ print(tf.test.gpu_device_name())
 
 from utils import *
 import pandas as pd
-import numpy as np
 from matplotlib import pyplot as plt
 import csv
 import math
@@ -42,46 +36,9 @@ import scipy as sp
 from tqdm import tqdm
 import json
 from tta import Test_Time_Augmentation
-
-# In[3]:
-
-
-# !rm -r food-101/
-
-
-# In[5]:
-
-
-# Helper function to download data and extract
 import os
-from IPython.display import clear_output
-# def get_data_extract():
-#   if "food-101" in os.listdir():
-#     print("Dataset already exists")
-#   else:
-#     print("Downloading the data...")
-#     get_ipython().system('wget http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz')
-#     print("Dataset downloaded!")
-#     print("Extracting data..")
-#     get_ipython().system('tar xzvf food-101.tar.gz')
-#     clear_output()
-#     print("Extraction done!")
+from tensorflow.keras.preprocessing import image
 
-
-# * **Grab a coffee, this is going to take some time..**
-
-# In[6]:
-
-
-# Download data and extract it to folder
-# get_data_extract()
-
-
-# In[7]:
-
-
-# get_ipython().system('rm food-101.tar.gz')
-# get_ipython().system('ls')
 
 
 # ### Understand dataset structure and files 
@@ -92,62 +49,28 @@ from IPython.display import clear_output
 # * **Note found on the webpage of the dataset :  **  
 # ***On purpose, the training images were not cleaned, and thus still contain some amount of noise. This comes mostly in the form of intense colors and sometimes wrong labels. All images were rescaled to have a maximum side length of 512 pixels.***  
 # * **The entire dataset is 5GB in size**
-
-# In[8]:
-
-
-# Check the extracted dataset folder
-# get_ipython().system('ls food-101/')
-
-
 # **images** folder contains 101 folders with 1000 images  each  
 # Each folder contains images of a specific food class
 
-# In[9]:
-
-
 import os
 os.listdir('food-101/images')
-
 
 # **meta** folder contains the text files - train.txt and test.txt  
 # **train.txt** contains the list of images that belong to training set  
 # **test.txt** contains the list of images that belong to test set  
 # **classes.txt** contains the list of all classes of food
 
-# In[10]:
-
-
 os.listdir('food-101/meta')
-
-
-# In[11]:
-
-
-# get_ipython().system('head food-101/meta/train.txt')
-
-
-# In[12]:
-
-
-# get_ipython().system('head food-101/meta/classes.txt')
 
 
 # ### Visualize random image from each of the 101 classes
 
-# In[13]:
-
-
 import matplotlib.pyplot as plt
 import matplotlib.image as img
-# get_ipython().run_line_magic('matplotlib', 'inline')
-import numpy as np
 from collections import defaultdict
 import collections
 import os
 import utils
-
-# In[14]:
 
 
 # Visualize the data, showing one image per class from 101 classes
@@ -178,9 +101,6 @@ plt.tight_layout()
 
 # ### Split the image data into train and test using train.txt and test.txt
 
-# In[15]:
-
-
 # Helper method to split dataset into train and test folders
 from shutil import copy
 from IPython.display import clear_output
@@ -202,15 +122,11 @@ def prepare_data(filepath, src,dest):
   print("Copying Done!")
 
 
-# In[16]:
-
 
 # Prepare train dataset by copying images from food-101/images to food-101/train using the file train.txt
 print("Creating train data...")
 # prepare_data('food-101/meta/train.txt', 'food-101/images', 'food-101/train')
 
-
-# In[17]:
 
 
 # Prepare test data by copying images from food-101/images to food-101/test using the file test.txt
@@ -218,21 +134,9 @@ print("Creating test data...")
 # prepare_data('food-101/meta/test.txt', 'food-101/images', 'food-101/test')
 
 
-# In[18]:
-
-
-# !rm -r food-101/images
-
-
-# In[19]:
-
-
 # Check how many files are in the train folder
 # print("Total number of samples in train folder")
 # get_ipython().system("find food-101/train -type d -or -type f -printf '.' | wc -c")
-
-
-# In[20]:
 
 
 # Check how many files are in the test folder
@@ -244,11 +148,6 @@ print("Creating test data...")
 
 
 # # ### Visualize the accuracy and loss plots
-
-# In[ ]:
-
-
-import matplotlib.pyplot as plt
 def plot_accuracy(history,title):
     plt.title(title)
     plt.plot(history.history['accuracy'])
@@ -256,14 +155,6 @@ def plot_accuracy(history,title):
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train_accuracy', 'validation_accuracy'], loc='best')
-    plt.show()
-
-def plot_prediction_accuracy(history,title):
-    plt.title(title)
-    plt.plot(history.history['accuracy'])
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['accuracy'], loc='best')
     plt.show()
 
 def plot_accuracy_csv_log(history_path,title):
@@ -289,14 +180,6 @@ def plot_loss(history,title):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train_loss', 'validation_loss'], loc='best')
-    plt.show()
-
-def plot_prediction_loss(history,title):
-    plt.title(title)
-    plt.plot(history.history['loss'])
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['loss'], loc='best')
     plt.show()
 
 def plot_loss_csv_log(history_path,title):
@@ -356,29 +239,6 @@ def plot_log(filename, show=True):
         plt.show()
 # # ### Predicting classes for new images from internet using the best trained model
 
-# In[ ]:
-
-
-# # Loading the best saved model to make predictions
-# %%time
-# import tensorflow.keras.backend as K
-# from tensorflow.keras.models import load_model
-
-# K.clear_session()
-# model_best = load_model('best_model_3class.hdf5',compile = False)
-
-
-# # * **Setting compile=False and clearing the session leads to faster loading of the saved model**
-# # * **Withouth the above addiitons, model loading was taking more than a minute!**
-
-# In[ ]:
-
-
-from tensorflow.keras.preprocessing import image
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-
 def predict_class(model, images, show = True):
   for img in images:
     img = image.load_img(img, target_size=(299, 299))
@@ -397,46 +257,6 @@ def predict_class(model, images, show = True):
         plt.show()
 
 
-# In[ ]:
-
-
-# # Downloading images from internet using the URLs
-# get_ipython().system('wget -O samosa.jpg http://veggiefoodrecipes.com/wp-content/uploads/2016/05/lentil-samosa-recipe-01.jpg')
-# get_ipython().system('wget -O pizza.jpg https://images.happycow.net/venues/500/46/64/hcmp46647_697901.jpeg')
-# get_ipython().system('wget -O omelette.jpg https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-an-omelette-horizontal-1542310072.png')
-
-# # If you have an image in your local computer and want to try it, uncomment the below code to upload the image files
-
-# # from google.colab import files
-# # image = files.upload()
-
-
-# In[ ]:
-
-
-# # Make a list of downloaded images and test the trained model
-# images = []
-# images.append('samosa.jpg')
-# images.append('pizza.jpg')
-# images.append('omelette.jpg')
-# predict_class(model_best, images, True)
-
-
-# # * **Yes!!! The model got them all right!!**
-
-# ### Fine tune Inceptionv3 model with 11 classes of data
-
-# * **We trained a model on 3 classes and tested it using new data**
-# * ** The model was able to predict the classes of all three test images correctly**
-# * **Will it be able to perform at the same level of accuracy for more classes?**
-# * **FOOD-101 dataset has 101 classes of data**
-# * ** Even with fine tuning using a pre-trained model, each epoch was taking more than an hour when all 101 classes of data is used(tried this on both Colab and on a Deep Learning VM instance with P100 GPU on GCP)**
-# * **But to check how the model performs when more classes are included, I'm using the same model to fine tune and train on 11 randomly chosen classes**
-# 
-
-# In[25]:
-
-
 # Helper function to select n random food classes
 import random
 def pick_n_random_classes(n):
@@ -450,56 +270,16 @@ def pick_n_random_classes(n):
   
 
 
-# In[26]:
-
 
 # Lets try with more classes than just 3. Also, this time lets randomly pick the food classes
 n = 101
-food_list = pick_n_random_classes(n)
+food_list = foods_sorted#pick_n_random_classes(n)
 
-
-# In[20]:
-
-
-# Create the new data subset of n classes
-# print("Creating training data folder with new classes...")
-# dataset_mini(food_list, src_train, dest_train)
-
-
-# In[21]:
-
-
-# print("Total number of samples in train folder")
-# get_ipython().system("find food-101/train_mini -type d -or -type f -printf '.' | wc -c")
-
-
-# In[ ]:
 
 
 # print("Creating test data folder with new classes")
 # dataset_mini(food_list, src_test, dest_test)
 
-
-# In[ ]:
-
-
-# print("Total number of samples in test folder")
-# get_ipython().system("find food-101/test_mini -type d -or -type f -printf '.' | wc -c")
-
-
-# In[ ]:
-
-
-# get_ipython().system('ls food-101/')
-
-
-# In[ ]:
-
-
-n = 101
-
-
-# In[28]:
 
 
 # Let's use a pretrained Inceptionv3 model on subset of data with 11 food classes
@@ -516,7 +296,6 @@ from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 from tensorflow.keras.optimizers import SGD, Adam
 from tensorflow.keras.regularizers import l2
 from tensorflow import keras
-import numpy as np
 
 K.clear_session()
 
@@ -531,19 +310,7 @@ nb_validation_samples = 25250 #2750 #25250
 batch_size = 16
 
 
-
-train_datagen = Rand_Augment(Numbers=2, max_Magnitude=10)
-
-
 test_datagen = ImageDataGenerator()
-
-
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='categorical')
-
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
@@ -553,8 +320,7 @@ validation_generator = test_datagen.flow_from_directory(
 
 
 inception = InceptionV3(weights='imagenet', include_top=False)
-# efficient = keras.applications.EfficientNetB3(include_top=False,
-#                                                 weights='imagenet', drop_connect_rate=0.4)
+
 
 rescale = keras.models.Sequential([
     keras.layers.experimental.preprocessing.Rescaling(1./255)
@@ -568,16 +334,10 @@ model.add(Dropout(0.2))
 model.add(Dense(n,kernel_regularizer=regularizers.l2(0.005), activation='softmax'))
 
 
-# model = keras.models.load_model('best_model_101class.hdf5')
-
-# In[ ]:
-
-
 # Loading the best saved model to make predictions
 from tensorflow.keras.models import load_model
 K.clear_session()
-model_best = load_model('best_model_101class.hdf5',compile = False)
-# In[ ]:
+model_best = load_model('best_model_101class_rand_augment_final.hdf5',compile = False)
 y_trues = []
 y_true = []
 batches = 0
@@ -594,7 +354,6 @@ for gen in tqdm(validation_generator):
 y_trues.extend(y_true)
 
 
-# In[ ]:
 predictions = []
 acc_history = {}
 
@@ -641,35 +400,8 @@ for idx, transform in enumerate(transforms):
 
 predictions = np.stack(predictions)
 
-plot_prediction_accuracy(prediction,'FOOD101-InceptionV3')
-plot_prediction_loss(prediction,'FOOD101-InceptionV3')
 # acc_history = agg_preds(predictions)
-# In[ ]:
-
 
 plot_log('history.log')
 plot_accuracy_csv_log('history.log','FOOD101-InceptionV3')
 plot_loss_csv_log('history.log','FOOD101-InceptionV3')
-
-
-
-# In[ ]:
-
-# If you have an image in your local computer and want to try it, uncomment the below code to upload the image files
-
-
-# from google.colab import files
-# image = files.upload()
-
-
-# In[ ]:
-
-
-# Make a list of downloaded images and test the trained model
-images = []
-images.append('data/frenchfries.jpg')
-images.append('data/cupcake.jpg')
-images.append('data/falafel.jpg')
-predict_class(model_best, images, True)
-
-
